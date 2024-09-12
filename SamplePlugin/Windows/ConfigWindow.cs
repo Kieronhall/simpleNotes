280dd3631e02e11a4336a17ca0c93d88114fd571
Kieronhall <kieron.hall2@mail.bcu.ac.uk>
2024-09-12 17:27:42 +0100

    Text Window Added, Lock Window Button Added
    Added text window which changes to size of the window, lock window button which locks position of window on screen.

--- SamplePlugin/Windows/ConfigWindow.cs
+++ SamplePlugin/Windows/ConfigWindow.cs
@@ -1,59 +1,60 @@
-﻿using System;
+using System;
 using System.Numerics;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 
-namespace SamplePlugin.Windows;
-
-public class ConfigWindow : Window, IDisposable
+namespace SamplePlugin.Windows
 {
-    private Configuration Configuration;
-
-    // We give this window a constant ID using ###
-    // This allows for labels being dynamic, like "{FPS Counter}fps###XYZ counter window",
-    // and the window ID will always be "###XYZ counter window" for ImGui
-    public ConfigWindow(Plugin plugin) : base("A Wonderful Configuration Window###With a constant ID")
+    public class ConfigWindow : Window, IDisposable
     {
-        Flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
-                ImGuiWindowFlags.NoScrollWithMouse;
+        private Configuration Configuration;
 
-        Size = new Vector2(232, 90);
-        SizeCondition = ImGuiCond.Always;
-
-        Configuration = plugin.Configuration;
-    }
-
-    public void Dispose() { }
-
-    public override void PreDraw()
-    {
-        // Flags must be added or removed before Draw() is being called, or they won't apply
-        if (Configuration.IsConfigWindowMovable)
+        // We give this window a constant ID using ###
+        // This allows for labels being dynamic, like "{FPS Counter}fps###XYZ counter window",
+        // and the window ID will always be "###XYZ counter window" for ImGui
+        public ConfigWindow(Plugin plugin) : base("A Wonderful Configuration Window###With a constant ID")
         {
-            Flags &= ~ImGuiWindowFlags.NoMove;
-        }
-        else
-        {
-            Flags |= ImGuiWindowFlags.NoMove;
-        }
-    }
+            Flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
+                    ImGuiWindowFlags.NoScrollWithMouse;
 
-    public override void Draw()
-    {
-        // can't ref a property, so use a local copy
-        var configValue = Configuration.SomePropertyToBeSavedAndWithADefault;
-        if (ImGui.Checkbox("Random Config Bool", ref configValue))
-        {
-            Configuration.SomePropertyToBeSavedAndWithADefault = configValue;
-            // can save immediately on change, if you don't want to provide a "Save and Close" button
-            Configuration.Save();
+            Size = new Vector2(232, 90);
+            SizeCondition = ImGuiCond.Always;
+
+            Configuration = plugin.Configuration;
         }
 
-        var movable = Configuration.IsConfigWindowMovable;
-        if (ImGui.Checkbox("Movable Config Window", ref movable))
+        public void Dispose() { }
+
+        public override void PreDraw()
         {
-            Configuration.IsConfigWindowMovable = movable;
-            Configuration.Save();
+            // Flags must be added or removed before Draw() is being called, or they won't apply
+            if (Configuration.IsConfigWindowMovable)
+            {
+                Flags &= ~ImGuiWindowFlags.NoMove;
+            }
+            else
+            {
+                Flags |= ImGuiWindowFlags.NoMove;
+            }
+        }
+
+        public override void Draw()
+        {
+            // can't ref a property, so use a local copy
+            var configValue = Configuration.SomePropertyToBeSavedAndWithADefault;
+            if (ImGui.Checkbox("Random Config Bool", ref configValue))
+            {
+                Configuration.SomePropertyToBeSavedAndWithADefault = configValue;
+                // can save immediately on change, if you don't want to provide a "Save and Close" button
+                Configuration.Save();
+            }
+
+            var movable = Configuration.IsConfigWindowMovable;
+            if (ImGui.Checkbox("Movable Config Window", ref movable))
+            {
+                Configuration.IsConfigWindowMovable = movable;
+                Configuration.Save();
+            }
         }
     }
 }
--- SamplePlugin/Windows/MainWindow.cs
+++ SamplePlugin/Windows/MainWindow.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Numerics;
 using Dalamud.Interface.Internal;
 using Dalamud.Interface.Utility;
@@ -6,53 +6,91 @@ using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using ImGuiNET;
 
-namespace SamplePlugin.Windows;
-
-public class MainWindow : Window, IDisposable
+namespace SamplePlugin.Windows
 {
-    private string GoatImagePath;
-    private Plugin Plugin;
-
-    // We give this window a hidden ID using ##
-    // So that the user will see "My Amazing Window" as window title,
-    // but for ImGui the ID is "My Amazing Window##With a hidden ID"
-    public MainWindow(Plugin plugin, string goatImagePath)
-        : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    public class MainWindow : Window, IDisposable
     {
-        SizeConstraints = new WindowSizeConstraints
+        private string IconImagePath;
+        private Plugin Plugin;
+
+        private bool IsMainWindowWindowMovable = true;
+        private Configuration Configuration;
+
+        // Variable to hold the user's input text
+        private string userInput = string.Empty;
+
+        public MainWindow(Plugin plugin, string iconImagePath)
+            : base("frimpyNotes", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
         {
-            MinimumSize = new Vector2(375, 330),
-            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
-        };
+            SizeConstraints = new WindowSizeConstraints
+            {
+                MinimumSize = new Vector2(375, 330),
+                MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+            };
 
-        GoatImagePath = goatImagePath;
-        Plugin = plugin;
-    }
-
-    public void Dispose() { }
-
-    public override void Draw()
-    {
-        ImGui.Text($"The random config bool is {Plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
-
-        if (ImGui.Button("Show Settings"))
-        {
-            Plugin.ToggleConfigUI();
+            IconImagePath = iconImagePath;
+            Plugin = plugin;
         }
 
-        ImGui.Spacing();
+        public void Dispose() { }
 
-        ImGui.Text("Have a goat:");
-        var goatImage = Plugin.TextureProvider.GetFromFile(GoatImagePath).GetWrapOrDefault();
-        if (goatImage != null)
+        public override void PreDraw()
         {
-            ImGuiHelpers.ScaledIndent(55f);
-            ImGui.Image(goatImage.ImGuiHandle, new Vector2(goatImage.Width, goatImage.Height));
-            ImGuiHelpers.ScaledIndent(-55f);
+            //Checking of window is movable or not
+            if (IsMainWindowWindowMovable)
+            {
+                Flags &= ~ImGuiWindowFlags.NoMove;
+            }
+            else
+            {
+                Flags |= ImGuiWindowFlags.NoMove;
+            }
         }
-        else
+
+        public override void Draw()
         {
-            ImGui.Text("Image not found.");
+            
+            //Lock Window Button
+            if (ImGui.Button("Lock Window"))
+            {
+                IsMainWindowWindowMovable = !IsMainWindowWindowMovable;
+                //Configuration.Save();
+            }
+
+            ImGui.Spacing();
+
+            var imageSize = new Vector2(50, 50);
+            var iconImage = Plugin.TextureProvider.GetFromFile(IconImagePath).GetWrapOrDefault();
+            if (iconImage != null)
+            {
+                var windowSize = ImGui.GetWindowSize();
+                // putting image top right corner
+                var imagePosition = new Vector2(
+                    windowSize.X - imageSize.X - 10, 
+                    30 
+                );
+                ImGui.SetCursorPos(imagePosition);
+                ImGui.Image(iconImage.ImGuiHandle, imageSize);
+            }
+            else
+            {
+                ImGui.Text("Image not found.");
+            }
+
+            ImGui.Spacing();
+
+            // Text Box
+            ImGui.Text("Enter some notes:");
+
+            var windowSize2 = ImGui.GetWindowSize();
+
+            var textBoxHeight = windowSize2.Y - ImGui.GetCursorPosY() - ImGui.GetFrameHeightWithSpacing(); // Remaining height minus the label height and some padding
+            var textBoxSize = new Vector2(windowSize2.X - 20, textBoxHeight); // Adjust width and height
+
+            ImGui.InputTextMultiline("##UserInputTextBox", ref userInput, 1024, textBoxSize, ImGuiInputTextFlags.AllowTabInput);
+
+
+            ImGui.Spacing();
         }
     }
 }
